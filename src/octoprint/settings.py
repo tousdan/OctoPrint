6c006e4ff61506a6fb3743194bdd720492d8ce56
Mark Walker <markwal@hotmail.com>
2015-12-30 14:13:33 -0700

    Add RepRapPro style file opened to virtual printer

--- src/octoprint/settings.py
+++ src/octoprint/settings.py
@@ -291,6 +291,7 @@ default_settings = {
 			"okWithLinenumber": False,
 			"numExtruders": 1,
 			"includeCurrentToolInTemps": True,
+			"includeFilenameOnOpened": True,
 			"movementSpeed": {
 				"x": 6000,
 				"y": 6000,
